f19785de84e8363b9cb978cab52669945a63bcdb
ReeseHatfield <hatfield.69@wright.edu>
2024-08-28 08:32:21 -0400

    move bin dir

--- martin.sh
+++ martin.sh
@@ -5,16 +5,16 @@ CURRENT_DIR=$(pwd)
 
 mkdir -p "$CURRENT_DIR/out"
 
-mkdir -p "$CURRENT_DIR/martin/bin"
+mkdir -p "$CURRENT_DIR/bin"
 
 # store files in temp directory
 mkdir -p "$CURRENT_DIR/temp"
 
 cd "$CURRENT_DIR/martin"
 
-go build -o "$CURRENT_DIR/martin/bin/martin.exe" main.go
+go build -o "$CURRENT_DIR/bin/martin.exe" main.go
 
-"$CURRENT_DIR/martin/bin/martin.exe" # execute
+"$CURRENT_DIR/bin/martin.exe" # execute
 
 # cleanup temp files
 rm -rf "$CURRENT_DIR/temp"
