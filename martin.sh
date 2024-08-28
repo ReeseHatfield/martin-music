2e8dfd4cd2d45399d9621e09fcbe25e4877fbb64
ReeseHatfield <hatfield.69@wright.edu>
2024-08-28 08:30:58 -0400

    automate image out

--- martin.sh
+++ martin.sh
@@ -2,12 +2,21 @@
 
 CURRENT_DIR=$(pwd)
 
+
+mkdir -p "$CURRENT_DIR/out"
+
 mkdir -p "$CURRENT_DIR/martin/bin"
+
+# store files in temp directory
+mkdir -p "$CURRENT_DIR/temp"
 
 cd "$CURRENT_DIR/martin"
 
 go build -o "$CURRENT_DIR/martin/bin/martin.exe" main.go
 
 "$CURRENT_DIR/martin/bin/martin.exe" # execute
+
+# cleanup temp files
+rm -rf "$CURRENT_DIR/temp"
 
 cd "$CURRENT_DIR"
--- martin/core/core.go
+++ martin/core/core.go
@@ -13,6 +13,8 @@ import (
 
 const (
 	COVER_DIMENSIONS_PX = 800
+	TEMP_FILE_NAME      = "../temp/temp.png"
+	OUT_DIR             = "../out/"
 )
 
 type Core struct {
@@ -38,6 +40,7 @@ func (c *Core) GeneratePfp() error {
 		return errors.New("Err: Cover image not set")
 	}
 
+	// build behind image
 	coverPath, err := filepath.Abs(c.cover.Path)
 	if err != nil {
 		return err
@@ -49,6 +52,7 @@ func (c *Core) GeneratePfp() error {
 	moveDown := 800
 	moveLeft := 1600
 
+	//ffmpeg needs radians for rotation
 	rotationDegrees := 20.0
 	rotationRadians := rotationDegrees * (math.Pi / 180.0)
 
@@ -56,7 +60,7 @@ func (c *Core) GeneratePfp() error {
 	cmd := exec.Command("ffmpeg", "-y", "-i", coverPath,
 		"-vf", fmt.Sprintf("scale=800:800,pad=%d:%d:%d:%d:0x00000000,rotate=%f:ow=%d:oh=%d:c=0x00000000",
 			c.background.Width, c.background.Height, moveDown, moveLeft, rotationRadians, c.background.Width, c.background.Height),
-		"output.png")
+		TEMP_FILE_NAME)
 
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -64,6 +68,28 @@ func (c *Core) GeneratePfp() error {
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("sum went wrong")
+	}
+
+	fmt.Println(stdout.String())
+	fmt.Println(stderr.String())
+
+	// build in front image
+
+	//ffmpeg -i MartinListensToRealMusic.png -i output.png -filter_complex "" -c:v png pfp.png
+
+	// this shoudl be a constant somewhere
+	martinPath, err := filepath.Abs(c.background.Image.Path)
+	if err != nil {
+		return err
+	}
+
+	// need to make unique name for out file
+	cmd = exec.Command("ffmpeg", "-y", "-i", martinPath,
+		"-i", TEMP_FILE_NAME, "-filter_complex", "[1:v][0:v]overlay=0:0", "-c:v", "png", OUT_DIR+"pfp.png")
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("sum went wrong at the end")
 	}
 
 	fmt.Println(stdout.String())
